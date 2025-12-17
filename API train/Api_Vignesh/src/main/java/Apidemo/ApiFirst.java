b70ccfa74de909c8c1fb48ee7d6ff66d904c8ed1
Vigneshperumalsca <Vigneshperumal.s.ca@gmail.com>
2025-12-17 17:56:59 -0500

    api

--- API train/Api_Vignesh/src/main/java/Apidemo/ApiFirst.java
+++ API train/Api_Vignesh/src/main/java/Apidemo/ApiFirst.java
@@ -11,6 +11,43 @@ public class ApiFirst {
 Response response=RestAssured.get("https://app.reqres.in/getting-started");
 
 System.out.println(response.getStatusCode());
+
+
+//Sample OAuth Token Generation
+Response response = given()
+ .contentType("application/x-www-form-urlencoded")
+ .formParam("grant_type", "client_credentials")
+ .formParam("client_id", "client_id")
+ .formParam("client_secret", "client_secret")
+ .post("/oauth/token");
+
+String token = response.jsonPath().getString("access_token");
+//Sample API Test Case
+given()
+ .header("Authorization", "Bearer " + token)
+ .when()
+ .get("/v1/accounts")
+ .then()
+ .statusCode(200)
+ .time(lessThan(2000L));
+
+
+
+
+
+/*Sample Maven Dependencies
+<dependency>
+  <groupId>io.rest-assured</groupId>
+  <artifactId>rest-assured</artifactId>
+  <version>5.4.0</version>
+</dependency>
+<dependency>
+  <groupId>org.testng</groupId>
+  <artifactId>testng</artifactId>
+  <version>7.9.0</version>
+</dependency>*/
+
+
 	}
 
 }
